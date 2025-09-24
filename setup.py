486518d8ba4b412218f221c5e9abc11c325b19a9
Sofia Arranz Regidor <18sar4@queensu.ca>
2025-09-24 15:03:58 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="QwaveMPS",
+    version="1.0.0", 
+    author="Sofia Arranz Regidor",
+    author_email="18sar4@queensu.ca",
+    description="Package to solve waveguide QED problems using MPS",
+    url="https://github.com/SofiaArranzRegidor/QwaveMPS",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Visualization"
+    ],
+    license="GNU General Public License v3 (GPLv3)",
+    packages=find_packages(),        # Automatically find packages in your repo
+    install_requires=[               # Optional: dependencies
+        "requests", 
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',]
+)
