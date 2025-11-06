b585317b87b8eb7c064343886314b199de5db10d
Sofia Arranz Regidor <18sar4@queensu.ca>
2025-11-06 16:32:58 -0500

    Examples updated

--- Example.py
+++ Example.py
@@ -14,8 +14,10 @@ import QwaveMPS as QM
 
 """Symmetrical Solution"""
 
+#%%
 
-"""Choose the simulation parameters"""
+
+""""Choose the simulation parameters"""
 
 "Choose the time step and end time"
 
@@ -35,11 +37,9 @@ bond=8
 i_s0=QM.states.i_se()
 i_n0=QM.states.i_ng(d_t)
 
-# gamma_l,gamma_r=QM.coupling('chiral_l',gamma=1)
-# gamma_l,gamma_r=QM.coupling('chiral_r',gamma=1)
-# gamma_l,gamma_r=QM.coupling('symmetrical',gamma=1)
-# gamma_l,gamma_r=QM.coupling('other',gamma=1,gamma_l=0.6,gamma_r=0.4) #same as next line
-gamma_l,gamma_r=0.4,0.6
+
+gamma_l,gamma_r=QM.coupling('symmetrical',gamma=1)
+
 
 """Choose the Hamiltonian"""
 
@@ -61,8 +61,8 @@ pop,tbinsR,tbinsL,trans,ref,total=QM.pop_dynamics(sys_b,time_b,delta_t)
 plt.figure(figsize=(4.5,4))
 plt.plot(tlist,np.real(trans),linewidth = 3,color = 'orange',linestyle='-',label='Transmission')
 plt.plot(tlist,np.real(ref),linewidth = 3,color = 'b',linestyle=':',label='Reflection')
+plt.plot(tlist,np.real(pop),linewidth = 3, color = 'k',linestyle='-',label=r'$n_{TLS}$')
 plt.plot(tlist,np.real(total),linewidth = 3,color = 'g',linestyle='-',label='Total')
-plt.plot(tlist,np.real(pop),linewidth = 3, color = 'k',linestyle='-',label='TLS pop')
 # plt.plot(tlist,np.real(tbinsR)/delta_t,linewidth = 2,color = 'r',linestyle='--',label=r'$n_R/dt$')
 plt.legend(loc='upper right', bbox_to_anchor=(1, 0.95),labelspacing=0.2)
 plt.xlabel('Time, $\gamma t$')
@@ -72,4 +72,48 @@ plt.xlim([0.,tmax])
 plt.tight_layout()
 plt.show()
 
+
+#%%
+
+"""Right chiral Solution"""
+
+#%%
+
+
+""" Updated coupling"""
+
+gamma_l,gamma_r=QM.coupling('chiral_r',gamma=1)
+
+
+
+"""Choose the Hamiltonian"""
+
+Hm=QM.hamiltonian_1TLS(delta_t, gamma_l, gamma_r)
+
+
+"""Calculate time evolution of the system"""
+
+sys_b,time_b = QM.t_evol_m(Hm,i_s0,i_n0,delta_t,tmax,bond,d_sys,d_t)
+
+
+"""Calculate population dynamics"""
+
+pop,tbinsR,tbinsL,trans,ref,total=QM.pop_dynamics(sys_b,time_b,delta_t)
+
+
+#%%
+
+plt.figure(figsize=(4.5,4))
+plt.plot(tlist,np.real(trans),linewidth = 3,color = 'orange',linestyle='-',label='Transmission')
+plt.plot(tlist,np.real(ref),linewidth = 3,color = 'b',linestyle=':',label='Reflection')
+plt.plot(tlist,np.real(pop),linewidth = 3, color = 'k',linestyle='-',label=r'$n_{TLS}$')
+plt.plot(tlist,np.real(total),linewidth = 3,color = 'g',linestyle='-',label='Total')
+# plt.plot(tlist,np.real(tbinsR)/delta_t,linewidth = 2,color = 'r',linestyle='--',label=r'$n_R/dt$')
+plt.legend(loc='center right')
+plt.xlabel('Time, $\gamma t$')
+plt.ylabel('Population')
+plt.ylim([0.,1.05])
+plt.xlim([0.,tmax])
+plt.tight_layout()
+plt.show()
 
--- Example_1TLS_feedback.py
+++ Example_1TLS_feedback.py
@@ -12,17 +12,34 @@ import QwaveMPS as QM
 
 #%%
 
+"""One first need to choose the delay time tau:
+
+In this case it is the roundtrip time of the feedback loop (back and forth from the mirror)
+
+"""
+
+tau=0.5
+
+
+#%%
+
+""" Example with constructive feedback:
+
+Choose a constructive feedback phase"""
+
+phase=np.pi
+
+#%%
+
 """Choose the time step and end time"""
 
-delta_t = 0.1
+delta_t = 0.03
 tmax = 5
 tlist=np.arange(0,tmax+delta_t,delta_t)
 d_sys=2
 d_t=2
 
-"""Choose the delay time"""
 
-tau=0.5
 
 """ Choose the initial state and coupling"""
 
@@ -33,16 +50,14 @@ i_n0=QM.states.i_ng(d_t)
 gamma_l,gamma_r=QM.coupling('symmetrical',gamma=1)
 
 
-phase=np.pi
+""" Choose max bond dimension"""
+
+bond=16
+
 
 """Choose the Hamiltonian"""
 
 Hm=QM.hamiltonian_1TLS_feedback(delta_t,gamma_l,gamma_r,phase,d_t,d_sys)
-
-
-""" Choose max bond dimension"""
-
-bond=16
 
 
 """ Time evolution of the system"""
@@ -61,8 +76,48 @@ plt.figure(figsize=(4.5,4))
 plt.plot(tlist,np.real(pop),linewidth = 3, color = 'k',linestyle='-',label=r'$n_{\rm TLS}$')
 plt.plot(tlist,np.real(tbins)/delta_t,linewidth = 3,color = 'r',linestyle='-',label=r'$n_R/dt$')
 plt.plot(tlist,np.real(trans),linewidth = 3,color = 'orange',linestyle='-',label='T')
-plt.plot(tlist,np.real(ph_loop),linewidth = 3,color = 'b',linestyle=':',label='Ph. in loop')
-plt.plot(tlist,total,linewidth = 3,color = 'g',linestyle='-',label='Total')
+plt.plot(tlist,np.real(ph_loop),linewidth = 3,color = 'b',linestyle=':',label=r'$n_{\rm loop}$')
+plt.plot(tlist,np.real(total),linewidth = 3,color = 'g',linestyle='-',label='Total')
+plt.legend(loc='upper right', bbox_to_anchor=(1, 0.95),labelspacing=0.2)
+# plt.xlabel('Time, t$\gamma$')
+# plt.ylim([0.9,1.05])
+plt.xlim([0.,tmax])
+# plt.xlim([0.,0.5])
+plt.tight_layout()
+plt.show()
+
+
+#%%
+
+""" Example with destructive feedback
+
+Choose a destructive feedback phase"""
+
+phase=0
+
+"""Choose the Hamiltonian"""
+
+Hm=QM.hamiltonian_1TLS_feedback(delta_t,gamma_l,gamma_r,phase,d_t,d_sys)
+
+
+""" Time evolution of the system"""
+
+sys_b,time_b,tau_b = QM.t_evol_nm(Hm,i_s0,i_n0,tau,delta_t,tmax,bond,d_t,d_sys)
+
+
+""" Calculate population dynamics"""
+
+pop,tbins,trans,ph_loop,total=QM.pop_dynamics_1TLS_nm(sys_b,time_b,tau_b,tau,delta_t)
+
+
+#%%
+
+plt.figure(figsize=(4.5,4))
+plt.plot(tlist,np.real(pop),linewidth = 3, color = 'k',linestyle='-',label=r'$n_{\rm TLS}$')
+plt.plot(tlist,np.real(tbins)/delta_t,linewidth = 3,color = 'r',linestyle='-',label=r'$n_R/dt$')
+plt.plot(tlist,np.real(trans),linewidth = 3,color = 'orange',linestyle='-',label='T')
+plt.plot(tlist,np.real(ph_loop),linewidth = 3,color = 'b',linestyle=':',label=r'$n_{\rm loop}$')
+plt.plot(tlist,np.real(total),linewidth = 3,color = 'g',linestyle='-',label='Total')
 plt.legend(loc='upper right', bbox_to_anchor=(1, 0.95),labelspacing=0.2)
 # plt.xlabel('Time, t$\gamma$')
 # plt.ylim([0.9,1.05])
