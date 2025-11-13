454c085da4b93aea52ccb1079678d3dced780a25
MatthewKoz <71106679+MatthewKoz@users.noreply.github.com>
2025-11-13 12:23:23 -0500

    Fock Pulse Feature Added
    As discussed, included code and an example for a non-trivial input photonic field. Feel free to use all or none of the code in the branch (accept or reject). We can also use it as a base and create a wrapper function around it if we want to limit it's usage in some way while using the underlying function still.
    
    Also added a function to take a list of OC normalized bins and a list of operators and computed a list of time dependent expectation values of operators. I think moving to taking expectation values like this from a user side would be good, as it gives the user a lot more versatility in the operators/expectation values they take.

--- QwaveMPS/states.py
+++ QwaveMPS/states.py
@@ -7,6 +7,7 @@ This module contains initial states for the waveguide and the TLSs
 """
 
 import numpy as np
+from collections.abc import Iterator
 
 def i_ng(d_t:int, bond0:int=1) -> np.ndarray:
     """
@@ -31,6 +32,7 @@ def i_ng(d_t:int, bond0:int=1) -> np.ndarray:
     i= np.zeros([bond0,d_t,bond0],dtype=complex) 
     i[:,0,:]=1.
     return i
+
 
 def i_sg(d_sys1:int=2, bond0:int=1) -> np.ndarray:
     """
@@ -79,6 +81,49 @@ def i_se(d_sys1:int=2, bond0:int=1) -> np.ndarray:
     i_s = np.zeros([bond0,d_sys1,bond0],dtype=complex) 
     i_s[:,1,:]=1.
     return i_s
+
+def input_state_generator(d_t_total:list[int], input_bins:list[np.ndarray]=None, bond0:int=1, default_state=None) -> Iterator[np.ndarray]:
+    """
+    Creates an iterator (generator) for the input field states of the waveguide.
+
+    Parameters
+    ----------
+    d_t_total : list[int]
+        List of sizes of the photonic Hilbert spaces.
+
+    input_bins : list[np.ndarray], default: None
+        List of time bins describing the input field state.
+        
+    bond0 : int, default: 1
+        Size of the initial bond dimension.
+
+    default_state : ndarray, default: None
+        Default time bin state yielded as an input state after all input_bins are exhusted. If None then vacuum states are yielded.
+    
+    Returns
+    -------
+    gen : Generator
+        A generator for the input field time bins.
+    
+    Examples
+    -------- 
+    """ 
+    d_t = np.prod(d_t_total)
+    if input_bins == None:
+        input_bins = []
+        
+    for i in range(len(input_bins)):
+        yield input_bins[i]
+    
+    # After all specified input bins are yielded, start inputting vacuum bins
+    # Is there any reason we may want the default bin to be something else (an input parameter)?
+    if default_state is None:
+        while True:
+            yield i_ng(d_t, bond0)
+    else:
+        while True:
+            yield default_state
+
 
 def coupling(coupl:str='symmetrical', gamma:float=1,gamma_r=None,gamma_l=None) -> tuple[float,float]:
     """ Coupling can be chiral or symmetrical.
