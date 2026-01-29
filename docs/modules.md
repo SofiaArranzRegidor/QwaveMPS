ccc681ded081bd819721a94b787dccfde09de183
MatthewKoz <71106679+MatthewKoz@users.noreply.github.com>
2026-01-28 21:16:14 -0500

    Private Example fix and initial reference automation
    Added +1 to parents checked for the private examples so they can access the source code.
    
    Began initial developments for automation of the code reference for the documentation.

--- docs/modules.md
+++ docs/modules.md
@@ -6,4 +6,6 @@ These are the main modules and scripts in this repository:
 - `operators.py`: This module contains the main quantum operators, including the basic boson and TLS operators used to build the Hamiltonian, the time evolution operator, swap operator, and expectation and observable operators. 
 - `simulation.py`: This module contains the functions to apply SVDs, evolve the system both in the Markovian and non-Markovian regimes, and functions to calculate the main observables.
 - `states.py`: This module contains initial states for the waveguide and the TLSs.
+- `correlation.py`: This module contains functions to calculate two time point correlations.
+- `parameters.py`: This module contains InputParams data class used in the setup of the simulation, and the Bins data class that contains the results of the simulation.
 
---
+++ scripts/gen_ref_pages.py
@@ -0,0 +1,48 @@
+"""Generate the code reference pages."""
+
+from pathlib import Path
+
+root = Path(__file__).parent.parent
+src = root / "QwaveMPS/src"
+print("Directory: ",src)  
+import mkdocs_gen_files
+
+
+
+
+for path in sorted(src.rglob("*.py")):  
+    print(path)
+
+
+    module_path = path.relative_to(src).with_suffix("")  
+
+
+    doc_path = path.relative_to(src).with_suffix(".md")  
+
+
+    full_doc_path = Path("reference", doc_path)  
+
+
+
+    parts = tuple(module_path.parts)
+
+    if parts[-1] == "__init__": 
+        continue 
+        parts = parts[:-1]
+    elif parts[-1] == "__main__":
+        continue
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:  
+        prefix = "src."
+        identifier = prefix + ".".join(parts)  
+        print(identifier)
+
+
+        print("::: " + identifier, file=fd)  
+
+
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))  
+
+
+print("complete!")
