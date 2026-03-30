f61ce48229e54679a7aea5d049e46309792a2da7
MatthewKoz <71106679+MatthewKoz@users.noreply.github.com>
2026-03-30 15:15:17 -0400

    Shift back to global imports for convenience of prior examples

--- src/QwaveMPS/__init__.py
+++ src/QwaveMPS/__init__.py
@@ -3,24 +3,24 @@
 
 """
 QwaveMPS: Package to solve waveguide QED problems using MPS
-
 """
-'''
+
+#'''
 from QwaveMPS.correlation import *
 from QwaveMPS.hamiltonians import *
 from QwaveMPS.operators import *
 from QwaveMPS.parameters import *
 from QwaveMPS.simulation import *
 from QwaveMPS.states import *
-'''
 #'''
+'''
 from . import correlation
 from . import hamiltonians
 from . import operators
 from . import parameters
 from . import simulation
 from . import states
-#'''
+'''
 
 def cite():
     """ 
