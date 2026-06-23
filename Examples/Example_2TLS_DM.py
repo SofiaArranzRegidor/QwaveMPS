1786de70ba1de66153f1d0534de02362b36398b7
Matias Bundgaard-Nielsen <mabuni1998@gmail.com>
2026-06-23 13:43:44 +0200

    Include description of relative cutoff

--- Examples/Example_2TLS_DM.py
+++ Examples/Example_2TLS_DM.py
@@ -53,7 +53,7 @@ input_params = qmps.parameters.InputParams(
     bond_max=16,
     phase=0.0,
     tau=0.2,
-    relative_cutoff = 1e-7
+    relative_cutoff = 1e-7 # Relative cutoff of singular values, decrease if trace is not preserved together with increasing max bond dimension
 )
 
 tlist = np.arange(0, input_params.tmax + input_params.delta_t / 2, input_params.delta_t)
